2a9ea5eb9eb93c8cf02175104f8f5a2806a98aa5
sunildumpala <dumpala@gmail.com>
2024-01-19 11:19:25 -0800

    Part 1 complete

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("Let's get this party started!");
+
+async function getGif(searchText){
+  const res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchText}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+  console.log(res.data);
+}
+
+const form = document.querySelector('#searchform');
+form.addEventListener("submit", function (e) {
+  const input = document.querySelector('#search');
+  e.preventDefault();
+  console.log(input.value);
+  getGif(input.value);
+  input.value = '';
+})
